a31b9a44c2b19fa067824ebec84f097cb4635d87
Gabriel Freire <gabriel.freire@imsmaxims.com>
2019-04-24 23:06:50 +0100

    Ref: refactor on follower count, maybe better type

--- src/follower_count.jl
+++ src/follower_count.jl
@@ -7,11 +7,12 @@ include("utils.jl");
 
 struct InstagramProfile
     name::String
+    personal_url::String
     followers::Int32
 end
 
 function Base.print(value::InstagramProfile)
-    println("$(value.name) has $(value.followers) followers")
+    println("$(value.name) at $(value.personal_url) has $(value.followers) followers")
 end
 
 function Base.print(results::Array{InstagramProfile, 1})
@@ -20,9 +21,9 @@ function Base.print(results::Array{InstagramProfile, 1})
     end
 end
 
-function get_followers(url::String)::Int32
-    @info "Getting $url follower count"
+function get_followers(url::String)::InstagramProfile
     f_url = "https://www.instagram.com/" * url
+    @info "Fetching $f_url !"
     try
         r = get(f_url)
         if r.status == 200
@@ -37,14 +38,17 @@ function get_followers(url::String)::Int32
             # scripts[1]    = First script tag
             # scripts[1][1] = content of first script tag
             scripts_content = JSON.parse(strip(text(scripts[1][1])))
-            
             main_entity_of_page = scripts_content["mainEntityofPage"];
             interaction_statistics = main_entity_of_page["interactionStatistic"];
-            followers_count = interaction_statistics["userInteractionCount"];
-            return parse(Int32, followers_count)
+     
+            name = String(scripts_content["name"])
+            profile = String("url" in collect(keys(scripts_content)) ? scripts_content["url"] : "No personal URL")
+            followers_count = parse(Int32, interaction_statistics["userInteractionCount"]);
+            
+            return InstagramProfile(name, profile, followers_count)
         end
     catch e
-        print("Something wrong happened at $url :> $e");
+        print("Something wrong happened :> $e");
         return 0
     end
 end
@@ -53,7 +57,7 @@ function get_multiple_followers(profiles::Array{String, 1}, printable::Bool)::Ar
     arr::Array{InstagramProfile, 1} = []
     @time begin
         @sync for prof in profiles
-            @async push!(arr, InstagramProfile(prof, get_followers(prof)));
+            @async push!(arr, get_followers(prof));
         end
     end
     
